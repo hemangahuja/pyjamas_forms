926ff191337982c64389f04b8e7c471b04aa0f93
hemangahuja <hemangahujaeotw@gmail.com>
2023-04-06 00:03:37 +0530

    added load_balancer and logging

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask,request,render_template
+import logging
+import logging.config
+from flask import Flask, request, render_template
 import json
 from database import Database
 import crypto
@@ -11,18 +13,22 @@ db = Database(form["db_name"] + ".csv")
 app = Flask(__name__)
 
 
-@app.route("/",methods=["GET"])
+@app.before_request
+def log_request():
+    app.logger.debug("Request received: %s %s", request.method, request.url)
+
+
+@app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html",form_name = form['form_name'], fields = form['form'].keys())
-    
+    return render_template("index.html", form_name=form['form_name'], fields=form['form'].keys())
 
 
-@app.route("/submit",methods=["POST"])
+@app.route("/submit", methods=["POST"])
 def submit():
     row = {}
     print("request.form")
     print(request.form)
-    for form_field,config in form["form"].items():
+    for form_field, config in form["form"].items():
         data = request.form[form_field]
         if config['isEncrypted']:
             data = crypto.encrypt(data)
@@ -30,20 +36,26 @@ def submit():
     # print(row)
     db.write(row)
     # db.show()
-    
+
     return "submitted!"
 
-@app.route("/find",methods=["POST"])
+
+@app.route("/find", methods=["POST"])
 def find():
     try:
-        # for key in request.form:
-        #     if form['form'][key]:
-        #         request.form[key] = crypto.encrypt(request.form[key])
-        return db.find({key : {'data' : data,'isEncrypted' : form['form'][key]['isEncrypted']} for key,data in request.form.items()})
+        return db.find({key: {'data': data, 'isEncrypted': form['form'][key]['isEncrypted']} for key, data in request.form.items()})
     except Exception as e:
-        print("error",e)
+        print("error", e)
         return "Not Found"
 
-@app.route("/lookup",methods=["GET"])
+
+@app.route("/lookup", methods=["GET"])
 def lookup():
-    return render_template("lookup.html",form_name=form['form_name'],fields=[key for key,config in form['form'].items() if config['primaryKey']])
+    return render_template("lookup.html", form_name=form['form_name'], fields=[key for key, config in form['form'].items() if config['primaryKey']])
+
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(filename='error.log', level=logging.DEBUG)
+
+    app.run(host="0.0.0.0")
